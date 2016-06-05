1cc0b7feb0aa31e52bfee70d404d35f6128dc6f4
Saurav Wahid <saurav@medisys.com.sa>
2016-06-05 16:32:18 +0600

    16:32 PM

--- app/view/grid/RowExpander.js
+++ app/view/grid/RowExpander.js
@@ -1,6 +1,8 @@
 Ext.define('nstgrid.view.grid.RowExpander', {
     extend: 'Ext.grid.Panel',
-
+	requires: [
+      
+    ],
     xtype: 'row-expander-grid',
 	viewConfig    : {
         stripeRows    : true,
@@ -15,13 +17,22 @@ Ext.define('nstgrid.view.grid.RowExpander', {
     },
 	frame	: true,
 	border	: true,
-	width: 600,
-    height: 300,
+	width	: 600,
+    height	: 300,
+	width	: '100%',
+	selectRowOnExpand : true,
 	store   : 'nstgrid.store.Companies',
-    columns: [
-        { text: "configurationType",  dataIndex: 'configurationType', flex: 1},
-        { text: "company", dataIndex: 'company'},
-        { text: "actor", dataIndex: 'actor'}
-    ]
+	
+	plugins: [{
+        ptype: 'rowexpander',
+        rowBodyTpl : new Ext.XTemplate(
+            '<p><b>Company:</b> {company}</p>',
+            '<p><b>Actor:</b> {actor}</p>')
+    }],
+	columns: [
+		{ text: "company",flex: 1, dataIndex: 'company'},
+		{ text: "configurationType",  dataIndex: 'configurationType'},
+		{ text: "actor", dataIndex: 'actor'}
+	]
 
 });
